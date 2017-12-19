8cde433f400b08eda6532432ea58b6d895eb3128
gsaray <gsaray@mobify.com>
2017-12-18 17:58:01 -0800

    tweaks to match with design

---
+++ theme/js/version-picker.js
@@ -0,0 +1,70 @@
+function versionPicker(baseUrl, pathArray, changelogPath, currentVersion) {
+    console.log('pathArray: ', pathArray)
+    // remove version # from path and then create the path string
+    var absoluteUrl = pathArray.split(',').slice(1).join('/');
+
+    console.log('Base URL: ', baseUrl)
+    console.log('Absolute URL: ', absoluteUrl)
+
+    var versionsJsonUrl = baseUrl + 'assets/js/versions.json';
+    console.log('versionsJsonUrl: ', versionsJsonUrl)
+
+    var $versionPickerButton = $('button#version-picker');
+    var $versionPickerList = $('ul#version-picker-versions');
+    var $versionPickerCaret = $('#version-picker-caret');
+
+    var getVersionLabel = function(version, isLatest) {
+        return isLatest ? 'v' + version + ' latest' : 'v' + version
+    }
+
+    $versionPickerButton.click(function() {
+        $versionPickerButton.toggleClass('active');
+        $versionPickerCaret.toggleClass('active');
+        $versionPickerList.slideToggle(200);
+    });
+
+    $versionPickerList.click(function() {
+        $versionPickerButton.click();
+    });
+
+    var createItem = function(label, link, isSelected) {
+        var $item = $('<li class="c-version-picker__item">');
+        var $link = $('<a class="c-version-picker__link">' + label + '</a>');
+        if (isSelected) {
+            $link.addClass('c--selected');
+        } else {
+            $link.attr('href', link);
+        }
+
+        $item.append($link);
+        $versionPickerList.append($item);
+    }
+
+    $.get(versionsJsonUrl, function(versions) {
+        var numDisplayVersions = versions.length > 10 ? 10 : versions.length
+        for (var i = 0; i < numDisplayVersions; i++) {
+            var version = versions[i];
+            var isLatest = i === 0;
+            var isSelected = version === currentVersion ||
+                (isLatest && currentVersion === 'latest');
+            var versionLabel = getVersionLabel(version, isLatest);
+            createItem(versionLabel, baseUrl + version + '/' + absoluteUrl, isSelected);
+
+            if (isSelected) {
+                $versionPickerButton.text(getVersionLabel(version, isLatest))
+            }
+        }
+
+        if (versions.length > 10) {
+            createItem('Older versions', baseUrl + changelogPath);
+        }
+
+        // If the selected version is older than the first 10, it won't be in the list
+        // and our button label won't be setup
+        if ($versionPickerButton.text().length < 1) {
+            $versionPickerButton.text(getVersionLabel(currentVersion, false))
+        }
+
+        $versionPickerButton.show();
+    });
+}
